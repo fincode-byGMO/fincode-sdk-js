1afa7342533ca0f43bf86cdee2ca80dbd98bd50f
fincode-developer <fincode-developer@gmo-pg.com>
2023-01-11 14:17:01 +0900

    add: Card object

--- types/api/card.d.ts
+++ types/api/card.d.ts
@@ -1,3 +1,17 @@
 export namespace Card {
+    /**
+     * Card object
+     */
+    export type CardObject = {}    
+
+    /**
+     * Card brands user can use in fincode.
+     * 
+     * - `VISA`: Visa card.
+     * - `MASTER`: MasterCard card.
+     * - `JCB`: JCB card.
+     * - `AMEX`: American Express card.
+     * - `Diners`: DinersClub card.
+     */
     export type Brand = "VISA" | "MASTER" | "JCB" | "AMEX" | "DINERS"
 }
