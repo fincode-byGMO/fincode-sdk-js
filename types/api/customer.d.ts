5f5ef956a5bb54bbdca54370492a54ef46333082
fincode-developer <fincode-developer@gmo-pg.com>
2023-01-11 14:24:37 +0900

    add: POST /v1/customers req and res

--- types/api/customer.d.ts
+++ types/api/customer.d.ts
@@ -85,4 +85,75 @@ export namespace Customer {
          */
         card_registration: "0" | "1"
     }
+
+    /**
+     * Request object of Creating customer (used in POST /v1/customers)
+     */
+    export type CreatingRequest = {
+        /**
+         * Customer ID
+         */
+        id: string
+
+        /**
+         * Customer's name
+         */
+        name: string | null;
+        
+        /**
+         * Customer's email
+         */
+        email: string | null;
+
+        /**
+         * Country Code of customer's phone.
+         */
+        phone_cc: string | null;
+        
+        /**
+         * Number of customer's phone.
+         */
+        phone_no: string | null;
+        
+        /**
+         * City of the customer's billing address.
+         */
+        addr_city: string | null;
+        
+        /**
+         * ISO 3166-1 numeric country code of the customer's billing address.
+         */
+        addr_country: string | null;
+
+        /**
+         * 1st line of the customer's billing address.
+         */
+        addr_line1: string | null;
+        
+        /**
+         * 2nd line of the customer's billing address.
+         */
+        addr_line2: string | null;
+
+        /**
+         * 3rd line of the customer's billing address.
+         */
+        addr_line3: string | null;
+
+        /**
+         * Postal code of the customer's billing address.
+         */
+        addr_post_code: string | null;
+
+        /**
+         * ISO 3166-2 state code of the customer's billing address.
+         */
+        addr_state: string | null;
+    }
+    /**
+     * Response object of Creating customer (used in POST /v1/customers)
+     */
+    export type CreatingResponse = Readonly<_CreatingResponse>
+    type _CreatingResponse = CustomerObject
+
 }
